24ca1c2df53eb5b35bca8293e467c61c66d14c01
Fabian Aguario <fabian_aguario@outlook.com>
2021-04-29 20:13:10 -0500

    FEAT: Actividad9

---
+++ Actividad9/Ajedrez.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Variables</title>
+  </head>
+  <body>
+    <?php 
+        $a="911"
+        $b="789"
+        $c= "456"
+        $resultado= $a .$b .$c .$b .$b .$c .$a .$b .$c .$b .$a .$a .$b .$c .$b;
+            echo $resultado; 
+    ?> 
+  </body>
+</html>     
