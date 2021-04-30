c8b773464f413a227b3ca4f71134b54ce2b2ad65
Fabian Aguario <fabian_aguario@outlook.com>
2021-04-29 23:39:18 -0500

    FEAT: Actividad Ajedrez Verdadera

--- Actividad9/Ajedrez verdadero.php
+++ Actividad9/Ajedrez verdadero.php
@@ -5,21 +5,45 @@
     
       $blanco="blanco.jpg";
       $negro="negro.jpg";
-      $dimensiones=8;
+      $dimensiones=4;
       $dimensiones=$dimensiones/2;
-
+    
       for ($a=1; $a <=$dimensiones ; $a++) {
           echo "<tr>";
           for ($i=1; $i <=$dimensiones ; $i++) { 
               echo "<td>  <embed src=\"negro.jpg\" type=\"image/jpg\" width=\"50\" height=\"50\"> </td>";
               echo "<td>  <embed src=\"blanco.jpg\" type=\"image/jpg\" width=\"50\" height=\"50\"> </td>";
           }
+          if($dimensiones!=0)
+          {
+            echo "<td>  <embed src=\"negro.jpg\" type=\"image/jpg\" width=\"50\" height=\"50\"> </td>";
+          }
           echo "</tr>";
           for ($i=1; $i <=$dimensiones ; $i++) { 
               echo "<td>  <embed src=\"blanco.jpg\" type=\"image/jpg\" width=\"50\" height=\"50\"> </td>";
               echo "<td>  <embed src=\"negro.jpg\" type=\"image/jpg\" width=\"50\" height=\"50\"> </td>";
           }
+          if($dimensiones!=0)
+          {
+            echo "<td>  <embed src=\"blanco.jpg\" type=\"image/jpg\" width=\"50\" height=\"50\"> </td>";
+          }
+         
       }
+      if ($dimensiones%2!=0)
+      {
+        echo "<tr>";
+        for ($i=1; $i <=$dimensiones ; $i++) { 
+            echo "<td>  <embed src=\"negro.jpg\" type=\"image/jpg\" width=\"50\" height=\"50\"> </td>";
+            echo "<td>  <embed src=\"blanco.jpg\" type=\"image/jpg\" width=\"50\" height=\"50\"> </td>";
+        } 
+        if($dimensiones!=0)
+      {
+        echo "<td>  <embed src=\"negro.jpg\" type=\"image/jpg\" width=\"50\" height=\"50\"> </td>";
+      }
+        echo "</tr>";
+      }
+
+      
       echo "</table>";
        ?> 
   </table>
